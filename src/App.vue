cf95b5492c4f286eb3f0b045f9ceede1d580580b
streljk93 <strel.jk.93@gmail.com>
2019-06-02 13:55:45 +0300

    init project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>ЭКСПЕРТНАЯ СИСТЕМА </span>
+        <span class="subheading text-lowercase grey--text">[версия 0.0.1]</span>
+      </v-toolbar-title>
+    </v-toolbar>
+
+    <v-content>
+      <v-container>
+        <mn-question
+            name="Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusantium aliquam error illum iste libero nam nisi, odio odit quo reiciendis rem ut! Animi eos fugiat, fugit nam nemo numquam."
+            :answer-list="[{id: 1, name: 'answer 1'}]"
+        >
+        </mn-question>
+      </v-container>
+    </v-content>
+
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    import MnQuestion from "./components/MnQuestion";
+    export default {
+        name: 'App',
+        components: {
+            MnQuestion,
+        },
+        data () {
+            return {
+                //
+            }
+        }
+    }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/MnQuestion.vue
@@ -0,0 +1,38 @@
+<template>
+    <v-layout column>
+        <v-flex>
+            <v-card>
+                <v-card-text>
+                    {{name}}
+                </v-card-text>
+            </v-card>
+        </v-flex>
+        <v-flex>
+            <v-checkbox
+                v-for="(answer, i) in answerList"
+                :key="i"
+                :label="answer.name"
+            ></v-checkbox>
+        </v-flex>
+    </v-layout>
+</template>
+
+<script>
+    export default {
+        name: "MnQuestion",
+        props: {
+            name: {
+                type: String,
+                default: "",
+            },
+            answerList: {
+                type: Array,
+                default: () => [],
+            },
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
